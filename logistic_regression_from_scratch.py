bcba30b0a0ff9ae5f85272de22d246367392fca3
Alex Bi <alex.bi@klaviyo.com>
2019-12-28 11:40:12 -0500

    Add main function

--- logistic_regression_from_scratch.py
+++ logistic_regression_from_scratch.py
@@ -3,15 +3,19 @@ import numpy as np
 DEFAULT_COVARIANCE = [[1, 2], [2, 1]]
 
 
+def fit(X, y, weights, bias, num_iterations=1000, min_threshold=1e-4):
+    pass
+
+
 def predict(x, weights, intercept):
     p_success = np.array([_sigmoid(x.dot(weights) + intercept)])
     return np.where(p_success > 0.5, 1, 0)
 
 
-def _generate_data(random_seed=10):
+def _generate_data(n, random_seed):
     np.random.seed(random_seed)
-    a = np.random.multivariate_normal(mean=[0, 0], cov=DEFAULT_COVARIANCE, size=N)
-    b = np.random.multivariate_normal(mean=[2, 2], cov=DEFAULT_COVARIANCE, size=N)
+    a = np.random.multivariate_normal(mean=[0, 0], cov=DEFAULT_COVARIANCE, size=n)
+    b = np.random.multivariate_normal(mean=[2, 2], cov=DEFAULT_COVARIANCE, size=n)
     x = np.vstack((a, b))
     y = np.hstack((np.zeros(N), np.ones(N)))
     return x, y
@@ -23,3 +27,15 @@ def _cross_entropy_loss(y_pred, y_actual):
 
 def _sigmoid(x):
     return 1 / (1 + np.exp(-x))
+
+
+if __name__ == '__main__':
+    random_state = 10
+    n = 10000
+
+    np.random.seed(random_state)
+    X, y = _generate_data(n, random_state)
+
+    initial_weights = np.random.random((X.shape[1], 1))
+    initial_bias = 0.5
+
