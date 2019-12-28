f55f8314a430062b7265e9d55567f68c7b91ba4b
Alex Bi <alex.bi@klaviyo.com>
2019-12-28 10:56:08 -0500

    Function to generate random data from multivariate normal distributions

---
+++ logistic_regression_from_scratch.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+DEFAULT_COVARIANCE = [[1, 2], [2, 1]]
+
+
+def _generate_data(random_seed=10):
+    np.random.seed(random_seed)
+    a = np.random.multivariate_normal(mean=[0, 0], cov=DEFAULT_COVARIANCE, size=N)
+    b = np.random.multivariate_normal(mean=[2, 2], cov=DEFAULT_COVARIANCE, size=N)
+    x = np.vstack((a, b))
+    y = np.hstack((np.zeros(N), np.ones(N)))
+    return x, y
