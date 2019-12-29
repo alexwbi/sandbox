91a1946e227b3a07024779bafd7cbd52609af332
alex <alexwbi95@gmail.com>
2019-12-29 16:23:52 -0500

    Fill out method for updating parameters for each mini batch

--- mnist_neural_net.py
+++ mnist_neural_net.py
@@ -16,17 +16,33 @@ class NN(object):
             a = self._sigmoid(a.dot(b) + w)
         return a
 
-    def stochastic_gradient_descent(self, X, epochs, eta, mini_batch_size):
+    def stochastic_gradient_descent(self, training_data, epochs, eta, mini_batch_size):
         """
-        :param X: training data
+        :param training_data: training data
         :param epochs: number of epochs
         :param eta: learning rate
         :param mini_batch_size: mini batch size
         """
         for i in range(epochs):
-            np.random.shuffle(X)
-            mini_batches = np.array_split(X, mini_batch_size)
+            np.random.shuffle(training_data)
+            mini_batches = np.array_split(training_data, mini_batch_size)
             [self._calculate_gradient(mini_batch, eta) for mini_batch in mini_batches]
+
+    def _update_parameters(self, mini_batch, eta):
+        delta_b = [np.zeros(b.shape) for b in self.biases]
+        delta_w = [np.zeros(w.shape) for w in self.weights]
+        for x, y in mini_batch:
+            delta_b_point, delta_w_point = self._backpropagation(x, y)
+            delta_b = [b + d_b for b, d_b in zip(delta_b, delta_b_point)]
+            delta_w = [w + d_w for w, d_w in zip(delta_w, delta_w_point)]
+
+        batch_size = len(mini_batch)
+        # Divide by batch_size to average the changes across all datapoints in the batch
+        self.biases = [b - (eta / batch_size) * d_b for b, d_b in zip(self.biases, delta_b)]
+
+
+    def _backpropagation(self, x, y):
+        return x, y  # TODO
 
     def _initial_weights(self):
         num_neurons_in_consecutive_layers = zip(self.neurons_per_layer[:-1], self.neurons_per_layer[1:])
