d796ec59e3295d6d9659936db0539c807495b08b
alex <alexwbi95@gmail.com>
2019-12-29 15:22:30 -0500

    Fill out initial weights instance var

--- mnist_neural_net.py
+++ mnist_neural_net.py
@@ -8,13 +8,14 @@ class NN(object):
         self.num_layers = len(neurons_per_layer)
 
         np.random.seed(random_state)
-        # self.weights = [np.random]
-        # self.biases = np.random.randn()
+        self.weights = self._initial_weights()
+        self.biases = self._initial_biases()
+
+    def _initial_weights(self):
+        num_neurons_in_consecutive_layers = zip(self.neurons_per_layer[:-1], self.neurons_per_layer[1:])
+        return [np.random.standard_normal((y, x)) for x, y in num_neurons_in_consecutive_layers]
 
     def _initial_biases(self):
         non_initial_layer_neurons = self.neurons_per_layer[1:]
         biases = np.random.standard_normal(non_initial_layer_neurons.sum())
         return np.split(biases, non_initial_layer_neurons[:-1])
-
-
-
